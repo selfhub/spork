682cfbafc491236a4d934adee45bf89b39f4ee81
Douglas Eichelberger <dduugg@gmail.com>
2015-01-14 12:21:32 -0800

    update userDayStepsGenerator

--- userDayStepsGenerator.scala
+++ userDayStepsGenerator.scala
@@ -1,37 +1,26 @@
-import java.io.File
+import java.util.Calendar
+import java.util.concurrent.TimeUnit
 
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
 val DELIMITER = "#"
-val PREFIX = sys.env("AWS_BUCKET_PREFIX")
+val SCHEMA_PREFIX = "selfhub-io-schema-dduugg-" // sys.env("AWS_BUCKET_PREFIX")
 val SCHEMA_NAME = "fitbit"
+val FILES = "s3n://" + SCHEMA_PREFIX + SCHEMA_NAME + "/*"
 val STEP_SUM_INTERVAL = 1000 * 60 * 60 * 24
+val RESULTS_BUCKET = "selfhub-io-results-dduugg"
+val RESULTS_FOLDER = "fitbitUserDayStepCounts"
 
-val FILES = "s3n://" + PREFIX + SCHEMA_NAME + "/*"
+val timeStart = Calendar.getInstance.getTimeInMillis
 
 val sc = new SparkContext()
-val textFile = sc.textFile(FILES).cache()
-
-val OUTPUT_DIR = "temp"
-val DESTINATION = "fitbitUserDaySteps.csv"
-FileUtil.fullyDelete(new File(OUTPUT_DIR))
-FileUtil.fullyDelete(new File(DESTINATION))
-
-// merge sourcePath files into destPath file
-def merge(sourcePath: String, destPath: String): Unit =  {
-  val hadoopConfig = new Configuration()
-  val hdfs = FileSystem.get(hadoopConfig)
-  FileUtil.copyMerge(hdfs, new Path(sourcePath),
-    hdfs, new Path(destPath), false, hadoopConfig, null)
-}
+val textFile = sc.textFile(FILES)
 
 // create key-value pairs where keys are concatenations of user & day,
 // and values are the number of steps taken by the user on that day
 val userDaySteps = textFile.map(line => {
-  val columns = line split ","
+  val columns = line.split(",")
   val userDay = columns(0) + DELIMITER + (columns(1).toLong / STEP_SUM_INTERVAL)
   (userDay, columns(2).toLong)
 }).reduceByKey((x, y) => {
@@ -41,9 +30,11 @@ val userDaySteps = textFile.map(line => {
   Array(cols(0), cols(1).toLong * STEP_SUM_INTERVAL, tuple._2).mkString(",")
 })
 
-userDaySteps.saveAsTextFile(OUTPUT_DIR)
-merge(OUTPUT_DIR, DESTINATION)
-FileUtil.fullyDelete(new File(OUTPUT_DIR))
-FileUtil.fullyDelete(new File("." + DESTINATION + ".crc"))
+userDaySteps.saveAsTextFile("s3n://" + RESULTS_BUCKET + "/" + RESULTS_FOLDER)
 
-sys.exit
+val elapsedMillis = Calendar.getInstance.getTimeInMillis - timeStart
+val elapsedMins = TimeUnit.MINUTES.convert(elapsedMillis, TimeUnit.MILLISECONDS)
+
+println("Took " + elapsedMins + " minutes")
+
+//sys.exit
